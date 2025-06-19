3bc24e0ed0480f9e18db494f88c5798504220e98
Flippinpk <flippinpk@gmail.com>
2025-06-19 00:31:24 -0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+//middleware
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
+
+
+//routes
+const bookRoutes = require('./src/books/book.route')
+app.use("/api/books",bookRoutes)
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    app.use('/',( _req,res ) => {
+        res.send("Book store server is running");
+    });
+  }
+
+  main().then(()=> console.log("Mongodb connected successfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
